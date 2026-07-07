0455abad2a6b6b155e7a04399b6f1e9feef757e0
Pooja Rajkumar <pooja.rajkumar123@gmail.com>
2026-07-07 16:36:37 -0700

    [customer] adding additional info cases requested by customer - medications, reviewer expanded view of behavioral info, intake info

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import json
 
-from constants import GOOGLE_SHEET_KEY, GOOGLE_SHEET_KEY_SHELTER_LUV, GOOGLE_SHEET_KEY_SLACK
+from constants import GOOGLE_SHEET_KEY, GOOGLE_SHEET_KEY_BEHAVIORAL_FOSTER, GOOGLE_SHEET_KEY_BEHAVIORAL_FOSTER_INTAKE, GOOGLE_SHEET_KEY_FOSTER_QUESTIONAIRE, GOOGLE_SHEET_KEY_MEDICATIONS, GOOGLE_SHEET_KEY_SHELTER_LUV, GOOGLE_SHEET_KEY_SLACK
 from fastapi import FastAPI
 from google.oauth2.service_account import Credentials
 import gspread
@@ -17,9 +17,20 @@ def get_google_sheet(key: str):
     )
     client = gspread.authorize(creds)
     sheet = client.open_by_key(key)
-    sheet_data = sheet.get_worksheet(0).get_all_records()
-    print(f"{key} info:", sheet_data)
     return sheet
+
+@app.get("/behavioral/foster/{dog_name}") # behavioral outreach (foster) = intake
+def get_intake_info(dog_name: str):
+    # aggregate behavioral info from google sheet once workflow is up 
+    sheet = get_google_sheet(GOOGLE_SHEET_KEY_BEHAVIORAL_FOSTER_INTAKE)
+    sheet_data = sheet.get_worksheet(1).get_all_records()
+    pup_info = []
+    for row in sheet_data:
+        if row["Dog Name"].lower() == dog_name.lower():
+            pup_info.append(row)
+    # Add here if we want to include trainer info from behavioral foster intake sheet
+    return pup_info
+
 
 @app.get("/slack/{dog_name}")
 def get_slack_info(dog_name: str):
@@ -34,15 +45,27 @@ def get_slack_info(dog_name: str):
     return pup_info
 
 @app.get("/foster/{dog_name}")
-def get_foster_notes_questionaire_info(dog_name: str):
+def get_foster_notes_questionaire_info_reviewer_expanded(dog_name: str):
     # aggregate foster notes questionaire info from google sheet once workflow is up 
-    sheet = get_google_sheet(GOOGLE_SHEET_KEY)
-    sheet_data = sheet.get_worksheet(0).get_all_records()
+    sheet = get_google_sheet(GOOGLE_SHEET_KEY_FOSTER_QUESTIONAIRE)
+    sheet_data = sheet.get_worksheet(2).get_all_records()
     pup_info = []
     for row in sheet_data:
         if row["Mutt's Name"].lower() == dog_name.lower():
             pup_info.append(row)
     return pup_info
+
+@app.get("/medications/{dog_name}")
+def get_medications_info(dog_name: str):
+    # aggregate medications info from google sheet once workflow is up 
+    sheet = get_google_sheet(GOOGLE_SHEET_KEY_MEDICATIONS)
+    sheet_data = sheet.get_worksheet(0).get_all_records()
+    pup_info = []
+    for row in sheet_data:
+        if row["Dog's Name"].lower() == dog_name.lower():
+            pup_info.append(row)
+    return pup_info
+
 
 def get_shelter_luv_info():
     # aggregate shelter luv info from google sheet once workflow is up 
@@ -55,11 +78,17 @@ def get_shelter_luv_info():
 @app.get("/dog/{dog_name}")
 def get_dog_info(dog_name:str):
     slack_info = get_slack_info(dog_name)
-    foster_info = get_foster_notes_questionaire_info(dog_name)
+    intake_info = get_intake_info(dog_name)
+    reviewer_info = get_foster_notes_questionaire_info_reviewer_expanded(dog_name)
+    medications_info = get_medications_info(dog_name)
+    if not medications_info:
+        medications_info = "NONE"
     # shelter_luv_info = get_shelter_luv_info()  # Uncomment when implemented
     data = {
+        "intake_info": intake_info,
+        "reviewer_info": reviewer_info,
         "behavior_updates_slack": slack_info,
-        "foster_questionnaire_profile": foster_info,
+        "medications_info": medications_info,
         # "shelter_luv_info": shelter_luv_info,  # Uncomment when implemented
     }
     return Response(
